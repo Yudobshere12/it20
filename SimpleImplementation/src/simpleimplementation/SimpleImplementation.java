5956f59807e0364c8966f08b2c506c79820b1562
Yudobshere12 <yudobsheremijares@gmail.com>
2024-09-25 14:54:11 +0800

    IT20

---
+++ SimpleImplementation/src/simpleimplementation/SimpleImplementation.java
@@ -0,0 +1,15 @@
+
+package simpleimplementation;
+
+
+public class SimpleImplementation {
+
+
+    public static void main(String[] args) {
+        
+        int[] numbers = {10, 20, 30, 40, 50};
+        System.out.println("First element in array: " + numbers[2]);
+    }
+    }
+    
+
