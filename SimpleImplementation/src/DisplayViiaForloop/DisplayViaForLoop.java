3f577e96782efe383c9d37de1e314ea5268a7ef3
Yudobshere12 <yudobsheremijares@gmail.com>
2024-09-25 15:13:20 +0800

    DisplayViaForLoop

---
+++ SimpleImplementation/src/DisplayViiaForloop/DisplayViaForLoop.java
@@ -0,0 +1,15 @@
+package DisplayViaForLoop;
+
+public class DisplayViaForLoop {
+
+    public static void main(String[] args) {
+        String[] fruits = {"Apple", "Banana", "Orange", "Grapes"};
+
+        // Display array elements using for loop
+        System.out.println("Fruits in the array:");
+
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+}
