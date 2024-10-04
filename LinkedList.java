9199af04f2bd2a78c639b753d5643fc72991c131
x-markthespot <markyrasonabe2405@gmail.com>
2024-10-04 11:22:59 +0800

    added method to add a new node

--- LinkedList.java
+++ LinkedList.java
@@ -1,5 +1,24 @@
 package topic_2_linkedlist_int;
 
 public class LinkedList {
-    
+
+        //head of the list
+    private Node head;
+
+        //Method to add a new node
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+
+
 }
