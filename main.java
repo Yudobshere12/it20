18209a4e57707809ef4f34cb5f0bbf7ab98a3593
Yudobshere12 <yudobsheremijares@gmail.com>
2024-10-07 11:25:23 +0800

    added scanner

---
+++ main.java
@@ -0,0 +1,9 @@
+package topic_2_linkedlist_string;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        Scanner sc = new Scanner(System.in);
