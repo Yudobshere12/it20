fb6eaff865b011c184dca8415088704f2f0e9258
Yudobshere12 <yudobsheremijares@gmail.com>
2024-10-07 11:28:45 +0800

    added switch

--- main.java
+++ main.java
@@ -30,3 +30,52 @@ public class Main {
             System.out.print("Enter Your Choice: ");
             int choice = sc.nextInt();
             sc.nextLine();
+
+             switch (choice) {
+                case 1:
+                    System.out.print("Enter the song title to add: ");
+                    String songToAdd = sc.nextLine();
+                    list.add(songToAdd);
+                    System.out.println("Song added.");
+                    break;
+                    
+                     case 2:
+                    System.out.print("Enter the song title to delete: ");
+                    String songToDelete = sc.nextLine();
+                    System.out.print("Are you sure you want to delete \"" + songToDelete + "\"? (yes/no): ");
+                    String confirmation = sc.nextLine();
+                    if (confirmation.equalsIgnoreCase("yes")) {
+                        list.deleteByValue(songToDelete);
+                        System.out.println("Song deleted.");
+                    } else {
+                        System.out.println("Deletion canceled.");
+                    }
+                    break;
+                    
+                case 3: // Move or Swap
+                    System.out.print("Enter the first index: ");
+                    int index1 = sc.nextInt();
+                    System.out.print("Enter the second index: ");
+                    int index2 = sc.nextInt();
+                    System.out.print("Do you want to swap? (yes/no): ");
+                    String confirm = sc.next();
+                    boolean swap = confirm.equalsIgnoreCase("yes");
+                    list.moveOrSwapNodes(index1, index2, swap);
+                    System.out.println(swap ? "Songs swapped." : "dog name moved.");
+                    break;
+
+                    case 4:
+                    running = false;
+                    break;
+
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+        }
+
+        sc.close();
+        System.out.println("ogim!");
+        
+        }
+}
+    
